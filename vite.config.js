907670ee64877e332e915a2754d10aaada0e1f63
Chetankumar000 <chetanmistry000@gmail.com>
2025-03-26 23:43:21 +0530

    Prepare for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Change this if deploying to a subdirectory
   plugins: [react()],
-})
+});
